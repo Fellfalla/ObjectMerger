ec9cfdd0e5a33bfd71606a5ad208f72197367e39
Markus Weber <joop03@alice-dsl.net>
2016-12-23 22:16:11 +0100

    Extracted Attributes and put them into own project

--- Tapako.ObjectMerger/Properties/AssemblyInfo.cs
+++ Tapako.ObjectMerger/Properties/AssemblyInfo.cs
@@ -12,9 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyCompany("Markus Weber")]
-[assembly: AssemblyCopyright("Copyright © Markus Weber 2016")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
